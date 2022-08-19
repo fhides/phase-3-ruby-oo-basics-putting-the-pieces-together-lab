cd143ca07cd6767947c260fded75bedf41e7bbbc
fhidesjoseph <fhidesjoseph@gmail.com>
2022-08-19 09:44:37 +0300

    commit

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    attr_reader :title
+    attr_accessor :author , :page_count ,:genre
+    def initialize (title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
+book=Book.new ("JavaScript")
+book.author
+book.page_count
+book.genre
+book.turn_page
 
